f456347044d26c4212c9d5a42e43fc802c37aef9
Obasho10 <108071926+Obasho10@users.noreply.github.com>
2025-07-11 17:45:30 +0530

    Update utils.py

--- train/utils.py
+++ train/utils.py
@@ -5,23 +5,21 @@ import torch.nn.functional as F
 # Binary Cross Entropy loss with logits
 loss_object = nn.BCEWithLogitsLoss()
 
-def generator_loss(gen_output, target,disc_generated_output=None):
+def generator_loss(disc_generated_output, gen_output, target):
     # GAN loss
-    gan_loss=0
-    if(disc_generated_output is not None):
-        gan_loss = loss_object(disc_generated_output, torch.ones_like(disc_generated_output))
-    dr=100*torch.abs(torch.div((gen_output-target),target))
+    gan_loss = loss_object(disc_generated_output, torch.ones_like(disc_generated_output))
+
     # Mean absolute error (L1 loss)
-    l1_loss = F.l1_loss(gen_output, target,reduction='mean')
+    l1_loss = F.l1_loss(gen_output, target)
 
     # Euclidean (L2) loss
     r = target - gen_output
     l2_loss = torch.norm(r, p=2)
 
     # Total generator loss
-    total_gen_loss = l2_loss+gan_loss
+    total_gen_loss = gan_loss + 100 * l1_loss + l2_loss
 
-    return total_gen_loss, l1_loss
+    return total_gen_loss, gan_loss, l1_loss
 
 def discriminator_loss(disc_real_output, disc_generated_output):
     # Real loss
